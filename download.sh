15c39f6f600d53da7ead96351f15889fdc50a09c
jVillaveces <secevalliv@gmail.com>
2016-11-15 15:46:36 +0100

    restructured download

--- download.sh
+++ download.sh
@@ -1,37 +1,65 @@
 # Export java home
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_40.jdk/Contents/Home/
 
-# Remove tools folder
-rm -r -f tools*
-#rm -r -f logstash*
-#rm -r -f filebeat*
-#rm -r -f derby*
-#rm -r -f db-*ls
+# Download function
+Download () {
+  URL="$1"
+  FILENAME="tools/$(basename $URL)"
+
+  if [ -f $FILENAME ]; then
+    echo "File '$FILENAME' Exists"
+  else
+    wget $URL -P tools
+  fi
+}
 
 # Download Tools
+DownloadTools() {
 
-# Download ES
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.0.0.zip -P tools
-# Download logstash
-wget https://artifacts.elastic.co/downloads/logstash/logstash-5.0.0.zip -P tools
-# Download filebeat
-wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.0.0-darwin-x86_64.tar.gz -P tools
+  # Download ES
+  Download https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.0.0.zip
+  # Download logstash
+  Download https://artifacts.elastic.co/downloads/logstash/logstash-5.0.0.zip
+  # Download filebeat
+  Download https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.0.0-darwin-x86_64.tar.gz
 
-# Download derby
-wget http://ftp.fau.de/apache//db/derby/db-derby-10.13.1.1/db-derby-10.13.1.1-bin.zip -P tools
+  # Download derby
+  Download http://ftp.fau.de/apache//db/derby/db-derby-10.13.1.1/db-derby-10.13.1.1-bin.zip
+}
 
-# Unzip
-unzip -q tools/elasticsearch-5.0.0.zip -d tools
-unzip -q tools/logstash-5.0.0.zip -d tools
-unzip -q tools/db-derby-10.13.1.1-bin.zip -d tools
-tar -jxf tools/filebeat-5.0.0-darwin-x86_64.tar.gz --directory tools
+Unzip() {
 
-# Install logstash plugins
-./tools/logstash-5.0.0/bin/logstash-plugin install logstash-input-jdbc
-./tools/logstash-5.0.0/bin/logstash-plugin install logstash-output-jdbc
+  echo "Unzipping files"
 
-# Create database
-./tools/db-derby-10.13.1.1-bin/bin/ij ./config/db/db-schema.sql
+  # Unzip
+  unzip -o -q tools/elasticsearch-5.0.0.zip -d tools
+  unzip -o -q tools/logstash-5.0.0.zip -d tools
+  unzip -o -q tools/db-derby-10.13.1.1-bin.zip -d tools
+  tar -jxf tools/filebeat-5.0.0-darwin-x86_64.tar.gz --directory tools
+
+}
+
+ConfigLogstash () {
+
+  # Install logstash plugins
+  ./tools/logstash-5.0.0/bin/logstash-plugin install logstash-input-jdbc
+  ./tools/logstash-5.0.0/bin/logstash-plugin install logstash-output-jdbc
+
+}
+
+CreateDB () {
+
+  # Delete database folder
+  rm -r -f derbyDB
+  # Create database
+  ./tools/db-derby-10.13.1.1-bin/bin/ij ./config/db/db-schema.sql
+
+}
+
+DownloadTools
+Unzip
+ConfigLogstash
+CreateDB
 
 # Useful commands
 #./logstash/logstash-2.4.0/bin/logstash -f ./data/config/logstash.db.conf
